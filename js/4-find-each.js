8f7a1dd809fb2d087a5797fc9fce18baab0c6e54
Rafal Matalowski <rafal.matalowski@gmail.com>
2016-12-11 13:14:34 +0100

    finds
    i poczatek events

--- js/4-find-each.js
+++ js/4-find-each.js
@@ -1,4 +1,22 @@
-$(function() {
+$(function () {
+
+    $('body').find('p').css("color", "blue"); // jeden sposob na odwolanie sie i dodanie reguly css
+    $('body').find('p').css({'color': 'red'}); // drugi sposob na odwolanie i dodanie reguly
+    $('body').find('p').eq(2).css('color', "aqua"); // nadanie koloru 2 elementowi
+
+    $('p').each(function () { // wyswietla elementy
+        console.log(this);
+    });
+    
+    $('p').each(function (index) { //zlicza ile razy wykona sie petla
+        console.log(index);
+    });
+    
+    $('p').each(function(index){
+        $(this).addClass('pararaf-' + index);
+        
+    });
+    
 
 
 });
